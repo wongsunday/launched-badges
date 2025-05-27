df80855180afa2ab775dda4b0754d8d2762fe172
sunday.wong <sundaysdw@gmail.com>
2025-05-27 11:06:56 +0800

    Update star badge SVG dimensions for improved design consistency

--- packages/badges/src/generic/SocialBadge.tsx
+++ packages/badges/src/generic/SocialBadge.tsx
@@ -191,7 +191,7 @@ export const SocialBadge: React.FC<SocialBadgeProps> = ({
         );
       case 'star':
         return (
-          <svg x="14.5" y="-1" width="12" height="12" viewBox="0 0 16 16" fill="none" stroke={textColor} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+          <svg x="14.5" y="-1" width="12" height="12" viewBox="0 0 18 16" fill="none" stroke={textColor} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
             <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z" />
           </svg>
         );
